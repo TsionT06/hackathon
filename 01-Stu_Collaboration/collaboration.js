63bfed6578924426308aa45d4014047f940f4a02
TsionT06 <69204704+TsionT06@users.noreply.github.com>
2020-08-18 11:33:30 -0400

    hi

---
+++ 01-Stu_Collaboration/collaboration.js
@@ -0,0 +1,31 @@
+// GLOBAL VARIABLES
+//start question count by zero
+//start character score count by zero 
+var questionCount = 0;
+var spongebobScore = 0;
+var patrickScore = 0;
+var squidwardScore = 0;
+var planktonScore = 0;
+var garyScore = 0;
+var mrkrabsScore = 0;
+
+//Question 1
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+//Question 2
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q2a3 = document.getElementById("q2a3");
+var q2a4 = document.getElementById("q2a4");
+//Question 3
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
+//Question 4
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
